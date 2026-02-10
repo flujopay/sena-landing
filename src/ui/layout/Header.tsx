7b987e0730d109c6be1d4e599ba6bb96c1b29c8c
Diego Mateo Sandoval <diegoarmandomateosandoval@gmail.com>
2026-02-10 10:37:18 -0500

    feat: add recupera href

--- src/ui/layout/Header.tsx
+++ src/ui/layout/Header.tsx
@@ -26,7 +26,7 @@ export const Header = ({ variant }: Props) => {
     { id: 1, name: "Productos", href: "#productos", type: "scroll" as const },
     { id: 2, name: "Precios", href: "#precios", type: "scroll" as const },
     { id: 3, name: "Nosotros", href: "/nosotros", type: "redirect" as const },
-    { id: 4, name: "Recupera", href: "/recupera", type: "redirect" as const },
+    { id: 4, name: "Recupera", href: "https://recupera-landing.vercel.app/", type: "external" as const },
     { id: 5, name: "Blog", href: "/blog", type: "redirect" as const },
   ];
 
@@ -52,6 +52,8 @@ export const Header = ({ variant }: Props) => {
       }
     } else if (section.type === "redirect") {
       router.push(section.href);
+    } else if (section.type === "external") {
+      window.open(section.href, "_self");
     }
   };
 
