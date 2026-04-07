7d34bb55fa2d92d7fdd1bcb0c57fb1ce9b378560
EnriqueChavez09 <rchavezs@uni.pe>
2026-04-07 14:36:18 -0500

    feat: modify gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toast } from "@/ui/shared/Toast";
 import NinoChatInit from "@/ui/shared/NinoChatInit";
 import Whatsapp from "@/ui/shared/WhatsApp";
 import type { Metadata } from "next";
+import { GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
 import { Suspense } from "react";
 import { adobeCleanFont, canaroFont, caslonFont } from "./fonts";
@@ -48,21 +49,8 @@ export default async function RootLayout({
   return (
     <Providers country={country} countries={countries}>
       <html lang="es" dir="ltr">
+        <GoogleTagManager gtmId="GTM-T2QDCJ6C" />
         <head>
-          {/* Google Tag Manager Script (carga diferida) */}
-          <Script id="gtm-script" strategy="lazyOnload">
-            {`
-                      (function(w,d,s,l,i){
-                          w[l]=w[l]||[];
-                          w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                          var f=d.getElementsByTagName(s)[0],
-                          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                          j.async=true;
-                          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                          f.parentNode.insertBefore(j,f);
-                          })(window,document,'script','dataLayer','GTM-T2QDCJ6C');
-                          `}
-          </Script>
           {/* Google Ads */}
           <Script
             src="https://www.googletagmanager.com/gtag/js?id=AW-17962976949"
@@ -92,15 +80,6 @@ export default async function RootLayout({
               })();
             `}
           </Script>
-          {/* Fallback para Google Tag Manager */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-T2QDCJ6C"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
           <Suspense>{children}</Suspense>
           <ModalRenderer />
           <Toast />
